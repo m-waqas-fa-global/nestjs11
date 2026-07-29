8b499b72d060ba45358b382772a690a4ae20aef7
M Waqas <m.waqas@fam-llc.com>
2026-07-29 20:28:08 +0500

    Code Async

--- src/monitoring/services/monitoring.service.ts
+++ src/monitoring/services/monitoring.service.ts
@@ -8,7 +8,8 @@ export class MonitoringService {
   getLogs() {
     const filePath = path.join(process.cwd(), 'src', 'common', 'logs', 'api-log.json');
     const data = fs.readFileSync(filePath, 'utf8');
-    return JSON.parse(data);
+    const resData =   JSON.parse(data);
+    return resData.reverse();
   }
 
 }
